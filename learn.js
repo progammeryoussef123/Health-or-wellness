a53a7c36cee1ea6633d30305ad0251f951978fd8
progammeryoussef123 <ym688361@gmail.com>
2024-05-11 18:42:10 +0300

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,56 @@
+//لو لكتب عنصر يظهر ف المتصفح
+document.write('<h1>Holle World</h1>');
+// ازاى اعرف نوع اليبيانات
+console.log(typeof 'jo <br>');
+console.log(typeof 10);
+console.log(typeof ture)
+//ازاى اجمع البينات مع بعض
+let name ='youssef ';
+let age =18;
+console.log (`my name is ${name} is age ${age}`);
+// عمليه حسابيه لو مكش فاهم راجع الدرس
+let num1 ='6';
+let num2= '5';
+// كده حولت النص ال رقم 
+console.log (typeof +num1);
+// يس غير امانjavescript ظهور اكبر رقم ف 
+console.log (Number.MAX_VALUE);
+// javescript ظهور اكبر رقم امان ف 
+console.log (Number.MAX_SAFE_INTEGER);
+// javescript ظهور اقل رقم امان ف 
+console.log (Number.MIN_SAFE_INTEGER);
+// ازالى اعرف ان الرقم امان 
+console.log (Number.isSafeInteger(2006));
+// اعرف القيمه دى ازالى اننها صح او غلط
+console.log (Number.isInteger(2020));
+console.log (Number.isNaN(-1233));
+// تجويل اى الرقم سالب ال موجب
+console.log (Math.abs(-10));
+// الجذر التربيعى ل اى رقم 
+console.log (Math.sqrt(4));
+// اسس الرقم
+console.log (Math.pow(2,3));
+// int ال اقرب الرقمfloatتحويل الرقم 
+console.log (Math.round(10.5));
+//  بس لفوق round زى 
+console.log (Math.ceil(10.5));
+// يس لاقل رقمround عكس 
+console.log (Math.floor(10.5));
+// هيطلع اصغر رقم حت لو كان سالب
+console.log (Math.min(1,2,-3,4,5));
+//min هيطلع اكبر رقم عكس 
+console.log (Math.max(100,200,300));
+//string  تحويل الرقم ال 
+let x=100;
+console.log(typeof (1000).toString());
+// تكرار 
+let a='ali ';
+console.log (a.repeat(2))
+// let بيعد الحروف ال ف المتغير 
+console.log (a.length);
+//  2- هيسبها فاضيه undenfine معين الفرق م بينهم 1-هيقول index لو عايز اطيع 
+console.log (a[4]);
+console.log (a.charAt(4));
+let aaa='i love salam';
+console.log (aaa.slice());
+
